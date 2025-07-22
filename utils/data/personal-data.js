bfe8d570c98625490ce48d47a4442ea9ab57dc73
Vestertech <vesteerwood@gmail.com>
2025-07-22 09:09:27 -0400

    Updates my skill set and tried to modernize the responsiveness and features

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,9 +1,9 @@
 export const personalData = {
   name: "Sylvester Eziagor",
   profile: "/profile.png",
-  designation: "Software Developer",
+  designation: " CyberSecurity Analyst | Software Developer.",
   description:
-    "My name is Sylvester Eziagor. I am a professional and enthusiastic Developer and cyberSecurity enthusiast in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+    "Cyber Security Analyst with expertise in Software development, network security,Incident Response,threat detection, risk mitigation. Strong problem-solving and analytical skills, with a keen attention to detail in identifying and resolving vulnerabilities. Adept at troubleshooting, working under pressure, and effective communication to enhance security operations.",
   email: "ifeanyichukwu0102@gmail.com",
   phone: "+1(365)-883-7199",
   address: "1078, King street West, hamilton ON, Canada",
